dc2093751f4ca3543549e4140227bcf1a17859dd
Laura Pinheiro Marson <laurampinheiro17@gmail.com>
2025-09-08 10:53:14 -0300

    Criação do projeto

---
+++ CineTrack/CineTrack/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CineTrack
+//
+//  Created by Laura Pinheiro Marson on 07/09/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
